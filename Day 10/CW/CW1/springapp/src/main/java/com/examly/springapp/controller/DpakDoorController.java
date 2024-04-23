d3efa49f703736d624f484e712e4ea966d76b68b
DEEPAK-BARATHI <124178877+DEEPAK-BARATHI@users.noreply.github.com>
2024-04-23 12:44:56 +0530

    Add files via upload

---
+++ Day 10/CW/CW1/springapp/src/main/java/com/examly/springapp/controller/DpakDoorController.java
@@ -0,0 +1,100 @@
+package com.examly.springapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.model.DpakDoor;
+import com.examly.springapp.service.DpakDoorService;
+
+@RestController
+@RequestMapping("/api")
+public class DpakDoorController {
+
+    @Autowired
+    public DpakDoorService doorService;
+
+    @PostMapping("/door")
+    public ResponseEntity<DpakDoor> postData(@RequestBody DpakDoor door) {
+        DpakDoor doorData = doorService.postData(door);
+        if (doorData != null) {
+            return new ResponseEntity<>(doorData, HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @GetMapping("door/bycolor/{color}")
+    public ResponseEntity<List<DpakDoor>> getBycolor(@PathVariable String color) {
+        List<DpakDoor> door = doorService.getBycolor(color);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @PutMapping("door/{doorid}")
+    public ResponseEntity<DpakDoor> updateDetailResponseEntity(@PathVariable int doorid, @RequestBody DpakDoor door) {
+        DpakDoor doorData = doorService.updateDetail(doorid, door);
+        if (doorData != null) {
+            return new ResponseEntity<>(doorData, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("door/{doorid}")
+    public ResponseEntity<String> deleteDoor(@PathVariable int doorid) {
+        if (doorService.deleteDoor(doorid)) {
+            return new ResponseEntity<>("Door deleted successfully", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/door/bydoortype/{doortype}")
+    public ResponseEntity<List<DpakDoor>> getByDoor(@PathVariable String doortype) {
+        List<DpakDoor> door = doorService.getByDoorType(doortype);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/door")
+    public ResponseEntity<List<DpakDoor>> getAllDetails() {
+        List<DpakDoor> door = doorService.getAllDetails();
+        if (door != null) {
+
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+    @GetMapping("/door/{doorid}")
+    public ResponseEntity<DpakDoor> getDetailsById(@PathVariable int doorid) {
+        DpakDoor door = doorService.getDetailsById(doorid);
+        if (door != null) {
+            return new ResponseEntity<>(door, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
